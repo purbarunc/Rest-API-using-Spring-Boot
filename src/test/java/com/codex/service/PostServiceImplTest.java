41ae68d92be0baa874ea9e66a6bc26b6f74e9481
Purbarun <purbarun@gmail.com>
2021-07-17 20:11:11 +0530

    Added Junit Test Case for PostServiceImpl.

---
+++ src/test/java/com/codex/service/PostServiceImplTest.java
@@ -0,0 +1,37 @@
+package com.codex.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.codex.model.Post;
+import com.codex.model.User;
+import com.codex.repository.PostRepository;
+
+@SpringBootTest
+class PostServiceImplTest {
+	@InjectMocks
+	private PostServiceImpl postServiceImpl;
+	@Mock
+	private PostRepository postRepository;
+
+	@Test
+	@DisplayName("create is successful")
+	void create() {
+		when(postRepository.save(any())).thenReturn(getPost());
+		postServiceImpl.create(mock(Post.class));
+		verify(postRepository, times(1)).save(any());
+	}
+
+	private Post getPost() {
+		return Post.builder().id(100).userPosts("Test  Post").users(mock(User.class)).build();
+	}
+
+}
